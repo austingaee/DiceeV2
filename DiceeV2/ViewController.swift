121cf77faeec7ec6076c6f8ef4f514ae9bbab4cf
Austin <austingae@gmail.com>
2018-08-02 15:04:50 -0700

    Initial Commit

---
+++ DiceeV2/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  DiceeV2
+//
+//  Created by austin_gae on 8/2/18.
+//  Copyright © 2018 Austin Gae. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
